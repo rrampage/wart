63daca6dbf031db9421dda21f4a0326f7712e7b3
Raunak Ramakrishnan <raunak1001@gmail.com>
2023-11-17 01:30:48 +0530

    Functions

---
+++ wasm.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+type Instruction struct {
+	Op   string
+	Args []int
+}
+
+type Function struct {
+	NParams int
+	Returns bool
+	Code    []Instruction
+}
+
+func IntToBytes(val int) []byte {
+	buf := make([]byte, binary.MaxVarintLen32)
+	binary.PutVarint(buf, int64(val))
+	return buf
+}
+
+func BytesToInt(buf []byte) int {
+	v64, n := binary.Varint(buf)
+	if n > 4 {
+		panic(fmt.Sprintf("Too many bytes!"))
+	}
+	return int(v64)
+}
+
+func NewFunction(nparams int, returns bool, code []Instruction) *Function {
+	return &Function{
+		NParams: nparams,
+		Returns: returns,
+		Code:    code,
+	}
+}
+
+type Machine struct {
+	functions []*Function
+	memory    []byte
+	stack     []int // Assuming int for now
+	stackPtr  int
+}
+
+func NewMachine(functions []*Function, memSize int) *Machine {
+	s := make([]int, 0, 1024)
+	m := make([]byte, memSize)
+	return &Machine{
+		stack:     s,
+		memory:    m,
+		functions: functions,
+	}
+}
+
+func (m *Machine) Push(val int) {
+	l := len(m.stack)
+	if l > m.stackPtr {
+		m.stack[m.stackPtr] = val
+	} else {
+		m.stack = append(m.stack, val)
+	}
+	m.stackPtr += 1
+}
+
+func (m *Machine) Pop() int {
+	if m.stackPtr <= 0 {
+		panic("Stack underflow")
+	}
+	pop := m.stack[m.stackPtr-1]
+	m.stackPtr = m.stackPtr - 1
+	return pop
+}
+
+func (m *Machine) Load(addr int, offset int) []byte {
+	// Original: struct.unpack('<d', self.mem[addr:addr+8])[0]
+	return m.memory[addr : addr+offset]
+}
+
+func (m *Machine) Store(addr int, data []byte) {
+	// Original: self.mem[addr:addr+8] = struct.pack('<d', val)
+	for i, b := range data {
+		m.memory[addr+i] = b
+	}
+}
+
+func (m *Machine) Call(fun *Function, args []int) int {
+	m.Execute(fun.Code, args)
+	if fun.Returns {
+		retVal := m.Pop()
+		return retVal
+	}
+	return 0
+}
+
+func (m *Machine) exec(ins Instruction, locals []int) {
+	switch ins.Op {
+	case "const":
+		// TODO: length check
+		m.Push(ins.Args[0])
+	case "add":
+		left := m.Pop()
+		right := m.Pop()
+		res := left + right
+		fmt.Printf("Op: %s, Eval: %d + %d, Res: %d\n", ins.Op, left, right, res)
+		m.Push(res)
+	case "sub":
+		left := m.Pop()
+		right := m.Pop()
+		res := left - right
+		fmt.Printf("Op: %s, Eval: %d - %d, Res: %d\n", ins.Op, left, right, res)
+		m.Push(res)
+	case "mul":
+		left := m.Pop()
+		right := m.Pop()
+		res := left * right
+		fmt.Printf("Op: %s, Eval: %d * %d, Res: %d\n", ins.Op, left, right, res)
+		m.Push(res)
+	case "div":
+		left := m.Pop()
+		right := m.Pop()
+		res := left / right
+		fmt.Printf("Op: %s, Eval: %d / %d, Res: %d\n", ins.Op, left, right, res)
+		m.Push(res)
+	case "store":
+		// store val @addr
+		// Just storing int for now
+		val := m.Pop()
+		addr := m.Pop()
+		m.Store(addr, IntToBytes(val))
+		return
+	case "load":
+		addr := m.Pop()
+		loadedBytes := m.Load(addr, 4)
+		val := BytesToInt(loadedBytes)
+		m.Push(val)
+		return
+	case "local.get":
+		m.Push(locals[ins.Args[0]])
+		return
+	case "local.set":
+		val := m.Pop()
+		locals[ins.Args[0]] = val
+		return
+	case "call":
+		fun := m.functions[ins.Args[0]]
+		fargs := make([]int, fun.NParams)
+		for i, _ := range fargs {
+			// We need to add in reverse order as Pop is LIFO
+			fargs[fun.NParams-1-i] = m.Pop()
+		}
+		res := m.Call(fun, fargs)
+		fmt.Printf("Op: %s, Args: %v, Res: %d\n", ins.Op, fargs, res)
+		if fun.Returns {
+			m.Push(res)
+		}
+	default:
+		panic(fmt.Sprintf("Invalid instruction: %s args: %v", ins.Op, ins.Args))
+	}
+}
+
+func (m *Machine) Execute(instructions []Instruction, locals []int) {
+	for _, ins := range instructions {
+		fmt.Printf("Ins: %s, Args: %v, Stack: %v\n", ins.Op, ins.Args, m.stack[:m.stackPtr])
+		m.exec(ins, locals)
+	}
+}
+
+func main() {
+	xAddr := 0
+	yAddr := 4
+	xVal := 123
+	yVal := 234
+	ins := []Instruction{
+		{"const", []int{xAddr}},
+		{"const", []int{xVal}},
+		{"store", nil},
+		{"const", []int{yAddr}},
+		{"const", []int{yVal}},
+		{"store", nil},
+		{"const", []int{xAddr}},
+		{"load", nil},
+		{"const", []int{yAddr}},
+		{"load", nil},
+		{"sub", nil},
+	}
+
+	m := NewMachine(nil, 65536)
+	m.Execute(ins, nil)
+	fmt.Printf("%v\n", m.stack[:m.stackPtr])
+}
---
+++ wasm_test.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNoop(t *testing.T) {
+	assrt := assert.New(t)
+	// Do nothing
+	ins := []Instruction{}
+	m := NewMachine(nil, 0)
+	m.Execute(ins, nil)
+	assrt.Equal(0, m.stackPtr)
+}
+
+func TestConst(t *testing.T) {
+	assrt := assert.New(t)
+	// Just push a const
+	ins := []Instruction{
+		{"const", []int{1}},
+	}
+	m := NewMachine(nil, 0)
+	m.Execute(ins, nil)
+	assrt.Equal(1, m.stackPtr)
+}
+
+func TestAdd(t *testing.T) {
+	assrt := assert.New(t)
+	// Just push a const
+	ins := []Instruction{
+		{"const", []int{1}},
+		{"const", []int{111}},
+		{"add", nil},
+	}
+	m := NewMachine(nil, 0)
+	m.Execute(ins, nil)
+	assrt.Equal(1, m.stackPtr)
+	assrt.Equal(112, m.stack[0])
+}
+
+func TestLoadStore(t *testing.T) {
+	assrt := assert.New(t)
+	xAddr := 0
+	yAddr := 4
+	xVal := 123
+	yVal := 234
+	ins := []Instruction{
+		{"const", []int{xAddr}},
+		{"const", []int{xVal}},
+		{"store", nil},
+		{"const", []int{yAddr}},
+		{"const", []int{yVal}},
+		{"store", nil},
+		{"const", []int{xAddr}},
+		{"load", nil},
+		{"const", []int{yAddr}},
+		{"load", nil},
+		{"sub", nil},
+	}
+	m := NewMachine(nil, 65536)
+	m.Execute(ins, nil)
+	assrt.Equal(1, m.stackPtr)
+	assrt.Equal(111, m.stack[0])
+}
+
+func TestUpdatePositionLoadStore(t *testing.T) {
+	// x = x + v*dt
+	assrt := assert.New(t)
+	m := NewMachine(nil, 65536)
+	xAddr := 0
+	vAddr := 4
+	m.Store(xAddr, IntToBytes(200))
+	m.Store(vAddr, IntToBytes(3))
+	ins := []Instruction{
+		{"const", []int{xAddr}},
+		{"const", []int{xAddr}},
+		{"load", nil},
+		{"const", []int{vAddr}},
+		{"load", nil},
+		{"const", []int{1}},
+		{"mul", nil},
+		{"add", nil},
+		{"store", nil},
+	}
+	m.Execute(ins, nil)
+	assrt.Equal(0, m.stackPtr)
+	assrt.Equal(203, BytesToInt(m.memory[xAddr:xAddr+4]))
+}
+
+func TestUpdatePositionFunc(t *testing.T) {
+	// def updatePosition(x, v, dt):
+	//    return x + v*dt
+	assrt := assert.New(t)
+	updatePosition := NewFunction(3, true, []Instruction{
+		{"local.get", []int{0}}, // x
+		{"local.get", []int{1}}, // v
+		{"local.get", []int{2}}, // dt
+		{"mul", nil},            // v*dt
+		{"add", nil},            // x + v*dt
+	})
+	functions := []*Function{updatePosition}
+	m := NewMachine(functions, 65536)
+	xAddr := 0
+	vAddr := 4
+	m.Store(xAddr, IntToBytes(200))
+	m.Store(vAddr, IntToBytes(3))
+	ins := []Instruction{
+		{"const", []int{xAddr}},
+		{"const", []int{xAddr}},
+		{"load", nil},
+		{"const", []int{vAddr}},
+		{"load", nil},
+		{"const", []int{1}},
+		{"call", []int{0}}, // call updatePosition
+		{"store", nil},     // Set xAddr to function's return value
+	}
+	m.Execute(ins, nil)
+	assrt.Equal(0, m.stackPtr)
+	assrt.Equal(203, BytesToInt(m.memory[xAddr:xAddr+4]))
+}
