f8753e66acd46f216a68e6793897a2dde826cdea
Raunak Ramakrishnan <raunak1001@gmail.com>
2023-11-19 14:04:48 +0530

    More utility methods

--- waja/src/main/java/rrampage/waja/utils/ConversionUtils.java
+++ waja/src/main/java/rrampage/waja/utils/ConversionUtils.java
@@ -1,14 +1,42 @@
 package rrampage.waja.utils;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class ConversionUtils {
+    public static ByteBuffer bytesToBuffer(byte[] data) {
+        return ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+    }
     public static byte[] longToBytes(long val) {
-        return ByteBuffer.wrap(new byte[Long.BYTES]).putLong(val).array();
+        return bytesToBuffer(new byte[Long.BYTES]).putLong(val).array();
     }
 
     public static long bytesToLong(byte[] data) {
-        return ByteBuffer.wrap(data).getLong();
+        return bytesToBuffer(data).getLong();
+    }
+
+    public static byte[] intToBytes(int val) {
+        return bytesToBuffer(new byte[Integer.BYTES]).putInt(val).array();
+    }
+
+    public static int bytesToInt(byte[] data) {
+        return bytesToBuffer(data).getInt();
+    }
+
+    public static byte[] floatToBytes(float val) {
+        return bytesToBuffer(new byte[Float.BYTES]).putFloat(val).array();
+    }
+
+    public static float bytesToFloat(byte[] data) {
+        return bytesToBuffer(data).getFloat();
+    }
+
+    public static byte[] doubleToBytes(double val) {
+        return bytesToBuffer(new byte[Double.BYTES]).putDouble(val).array();
+    }
+
+    public static double bytesToDouble(byte[] data) {
+        return bytesToBuffer(data).getDouble();
     }
 
     public static int wrapBoolean(boolean val) {
