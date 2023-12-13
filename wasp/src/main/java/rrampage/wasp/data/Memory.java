97416823c9fa3a3461027bbcf614508a6d309ab1
Raunak Ramakrishnan <raunak1001@gmail.com>
2023-12-13 17:01:35 +0530

    Adding memory as a separate WASM data type

---
+++ wasp/src/main/java/rrampage/wasp/data/Memory.java
@@ -0,0 +1,45 @@
+package rrampage.wasp.data;
+
+import java.util.Arrays;
+
+public class Memory {
+    private static final int MEM_PAGE_SIZE = 65536;
+    private static final int MAX_PAGES = 256;
+    private byte[] memory;
+    public Memory(int pages) {
+        if (pages > MAX_PAGES) {
+            throw new RuntimeException(String.format("Can not allocate more than %d pages of memory", MAX_PAGES));
+        }
+        this.memory = new byte[pages * MEM_PAGE_SIZE];
+    }
+
+    public int getMemorySize() {
+        return memory.length/MEM_PAGE_SIZE;
+    }
+
+    public int grow(int numPages) {
+        int currPages = getMemorySize();
+        if (numPages < 0 || currPages + numPages > MAX_PAGES) {
+            return -1;
+        }
+        if (numPages == 0) {
+            return currPages;
+        }
+        byte[] newMemory = new byte[(currPages+numPages)*MEM_PAGE_SIZE];
+        System.arraycopy(memory, 0, newMemory, 0, memory.length);
+        memory = newMemory;
+        return -1;
+    }
+
+    public byte[] load(int addr, int offset) {
+        return Arrays.copyOfRange(memory, addr, addr + offset);
+    }
+
+    public void store(int addr, byte[] data) {
+        if (addr >= memory.length) {
+            throw new RuntimeException("Invalid address passed to memory: " + addr);
+        }
+        System.arraycopy(data, 0, memory, addr, data.length);
+    }
+
+}
