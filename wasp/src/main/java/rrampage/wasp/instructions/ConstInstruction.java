26cb41386df81d84eee430f65de756fb7f5c41f1
Raunak Ramakrishnan <raunak1001@gmail.com>
2024-01-23 17:16:11 +0530

    Typed select

--- wasp/src/main/java/rrampage/wasp/instructions/ConstInstruction.java
+++ wasp/src/main/java/rrampage/wasp/instructions/ConstInstruction.java
@@ -1,9 +1,29 @@
 package rrampage.wasp.instructions;
 
 public sealed interface ConstInstruction extends Instruction {
-    record IntConst(int val) implements ConstInstruction, ConstExpression { public String opCode() {return "i32.const";}}
-    record LongConst(long val) implements ConstInstruction { public String opCode() {return "i64.const";}}
-    record FloatConst(float val) implements ConstInstruction { public String opCode() {return "f32.const";}}
-    record DoubleConst(double val) implements ConstInstruction { public String opCode() {return "f64.const";}}
+    record IntConst(int val) implements ConstInstruction, ConstExpression {
+        public String opCode() {return "i32.const";}
+        public String toString() {return view();}
+    }
+    record LongConst(long val) implements ConstInstruction {
+        public String opCode() {return "i64.const";}
+        public String toString() {return view();}
+    }
+    record FloatConst(float val) implements ConstInstruction {
+        public String opCode() {return "f32.const";}
+        public String toString() {return view();}
+    }
+    record DoubleConst(double val) implements ConstInstruction {
+        public String opCode() {return "f64.const";}
+        public String toString() {return view();}
+    }
     static ConstInstruction[] of(ConstInstruction ... args) {return args;}
+    default String view() {
+        return switch (this) {
+            case DoubleConst i -> "f64:" + i.val();
+            case FloatConst i -> "f32:" + i.val();
+            case IntConst i -> "i32:" + i.val();
+            case LongConst i -> "i64:" + i.val();
+        };
+    }
 }
--- wasp/src/test/java/rrampage/wasp/data/AssertReturn.java
+++ wasp/src/test/java/rrampage/wasp/data/AssertReturn.java
@@ -2,4 +2,8 @@ package rrampage.wasp.data;
 
 import rrampage.wasp.instructions.ConstInstruction;
 
-public record AssertReturn(String function, ConstInstruction[] args, ConstInstruction[] expected) {}
+import java.util.Arrays;
+
+public record AssertReturn(String function, ConstInstruction[] args, ConstInstruction[] expected) {
+    public String toString() { return String.format("%s: Args: %s Expected: %s", function, Arrays.toString(args), Arrays.toString(expected));}
+}
