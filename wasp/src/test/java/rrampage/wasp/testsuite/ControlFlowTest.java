c15bf2860a0a51532ccaa10dea62ba866acc24bb
Raunak Ramakrishnan <raunak1001@gmail.com>
2024-01-28 23:54:03 +0530

    Using new test factory method

--- wasp/src/test/java/rrampage/wasp/testsuite/ControlFlowTest.java
+++ wasp/src/test/java/rrampage/wasp/testsuite/ControlFlowTest.java
@@ -8,6 +8,6 @@ import java.util.stream.Stream;
 import static rrampage.wasp.testsuite.TestSuiteRunner.wastTest;
 
 public class ControlFlowTest {
-    @TestFactory public Stream<DynamicTest> testBlock() {return wastTest("block.0.wasm", "block.json");}
-    @TestFactory public Stream<DynamicTest> testLoop() {return wastTest("loop.0.wasm", "loop.json");}
+    @TestFactory public Stream<DynamicTest> testBlock() {return wastTest("block.json");}
+    @TestFactory public Stream<DynamicTest> testLoop() {return wastTest("loop.json");}
 }
--- wasp/src/test/java/rrampage/wasp/testsuite/PrimitivesTest.java
+++ wasp/src/test/java/rrampage/wasp/testsuite/PrimitivesTest.java
@@ -7,8 +7,8 @@ import java.util.stream.Stream;
 import static rrampage.wasp.testsuite.TestSuiteRunner.wastTest;
 
 public class PrimitivesTest {
-    @TestFactory  public Stream<DynamicTest> testI32() { return wastTest("i32.0.wasm", "i32.json");}
-    @TestFactory  public Stream<DynamicTest> testI64() { return wastTest("i64.0.wasm", "i64.json");}
-    @TestFactory public Stream<DynamicTest> testF32() {return wastTest("f32.0.wasm", "f32.json");}
-    @TestFactory public Stream<DynamicTest> testF64() { return wastTest("f64.0.wasm", "f64.json");}
+    @TestFactory  public Stream<DynamicTest> testI32() { return wastTest("i32.json");}
+    @TestFactory  public Stream<DynamicTest> testI64() { return wastTest("i64.json");}
+    @TestFactory public Stream<DynamicTest> testF32() {return wastTest("f32.json");}
+    @TestFactory public Stream<DynamicTest> testF64() { return wastTest("f64.json");}
 }
